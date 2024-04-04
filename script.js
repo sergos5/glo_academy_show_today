269163eb25a2cd64bdc26fd8372308d468483be7
Sergey <sergos5@yandex.ru>
2024-04-04 21:30:19 +0300

    добрый день, сегодня...+ меняется все

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ const todeyContent = document.getElementById('todey');
 const timeContent = document.getElementById('time');
 const newYearContent = document.getElementById('new-year');
 
-
-let date = new Date();
-
-let getTimeOfDay = (hours) => {
-    if (hours >= 6 && hours < 10) return 'Доброе утро';
-    if (hours >= 10 && hours < 16) return 'Добрый день';
-    if (hours >= 16 && hours < 22) return 'Добрый вечер';
-    if (hours >= 22 || hours < 6) return 'Доброй ночи';
-};
-
 const weekday = [
     'воскресенье',
     'понедельник',
@@ -23,25 +13,34 @@ const weekday = [
     'суббота'
 ];
 
-let currentTime = () => {
-    let time = new Date();
-    timeContent.textContent = `Текущее время: ${time.toTimeString().substring(0, 8)}`;
-    setTimeout(currentTime, 1000);
-}
+let showData = () => {
+    let date = new Date();
 
-let getDaysBeforeNewYear = () => {
-    const arrDey = ['дней', 'день', 'дня'];
-    let daysBeforeNewYear = Math.floor((Date.parse('1 january 2025') - Date.now()) / 1000 / 60 / 60 / 24);
-    let showDey = (n) => {
-        if (n > 10 && n < 20) return arrDey[0];
-        if (n % 10 > 1 && n % 10 < 5) return arrDey[2];
-        if (n % 10 == 1) return arrDey[1];
-        return arrDey[0];
+    let getTimeOfDay = (hours) => {
+        if (hours >= 6 && hours < 10) return 'Доброе утро';
+        if (hours >= 10 && hours < 16) return 'Добрый день';
+        if (hours >= 16 && hours < 22) return 'Добрый вечер';
+        if (hours >= 22 || hours < 6) return 'Доброй ночи';
     };
-    return daysBeforeNewYear + " " + showDey(daysBeforeNewYear);
-}
 
-goodContent.textContent = getTimeOfDay(date.getHours());
-todeyContent.textContent = `Сегодня: ${weekday[date.getDay()]}.`;
-newYearContent.textContent = `До Нового Года осталось: ${getDaysBeforeNewYear()}`;
-currentTime();
+    let getDaysBeforeNewYear = () => {
+        const arrDey = ['дней', 'день', 'дня'];
+        let daysBeforeNewYear = Math.floor((Date.parse('1 january 2025') - Date.now()) / 1000 / 60 / 60 / 24);
+        let showDey = (n) => {
+            if (n > 10 && n < 20) return arrDey[0];
+            if (n % 10 > 1 && n % 10 < 5) return arrDey[2];
+            if (n % 10 == 1) return arrDey[1];
+            return arrDey[0];
+        };
+        return daysBeforeNewYear + " " + showDey(daysBeforeNewYear);
+    };
+
+    goodContent.textContent = getTimeOfDay(date.getHours());
+    timeContent.textContent = `Текущее время: ${date.toTimeString().substring(0, 8)}`;
+    todeyContent.textContent = `Сегодня: ${weekday[date.getDay()]}.`;
+    newYearContent.textContent = `До Нового Года осталось: ${getDaysBeforeNewYear()}`;
+
+    setTimeout(showData, 1000);
+};
+
+showData();
